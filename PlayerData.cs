2920a4f52c2f57a16c550f4ccbe05e6863011d06
liamc <liamc@LAPTOP-QD3THINV>
2021-05-31 20:58:31 +0100

    Add project files.

---
+++ PlayerData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ApexDataGrabber
+{
+    class PlayerData
+    {
+
+        // general player data
+        public string playerName;
+        public int level;
+        public bool isBanned;
+
+        // ranked game data
+        public double rankScore;
+        public string rankName;
+        public int rankDivision;
+        public string rankImg;
+
+        // assigning parsed data from API
+        public PlayerData(string playerData)
+        {
+            JObject data = JObject.Parse(playerData);
+
+            playerName = (string)data["global"]["name"];
+            level = (int)data["global"]["level"];
+            isBanned = (bool)data["global"]["bans"]["isActive"];
+
+            rankScore = (double)data["global"]["rank"]["rankScore"];
+            rankName = (string)data["global"]["rank"]["rankName"];
+            rankDivision = (int)data["global"]["rank"]["rankDiv"];
+            rankImg = (string)data["global"]["rank"]["rankImg"];
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace ApexDataGrabber
+{
+    
+    //public class Product
+    //{
+    //    public string Id { get; set; }
+    //    public string Name { get; set; }
+    //    public decimal Price { get; set; }
+    //    public string Category { get; set; }
+    //}
+    //#endregion
+
+    class Program
+    {
+    
+        static HttpClient client = new HttpClient();
+
+
+
+        // inputting a name, building a request, returning deserialised product
+        static async Task<PlayerData> GetPlayerStatsAsync(string playerName)
+        {
+            string authKey = "iaRXcyiPqDryd2bSZKCj";
+
+            // send api request
+            var data = await client.GetStringAsync("bridge?version=5&platform=PC&player=" + playerName + "&auth=" + authKey);
+            //var deserializedProduct = JsonConvert.DeserializeObject(data);
+
+            PlayerData playerData = new PlayerData(data);
+
+            return playerData;
+
+        }
+
+
+
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://api.mozambiquehe.re/");
+            client.DefaultRequestHeaders.Accept.Clear();
+
+
+            try
+            {
+
+                PlayerData data = await GetPlayerStatsAsync("RatQueenLily");
+                
+
+
+
+                Console.WriteLine(data);
+
+
+            }
+            catch (Exception e)
+            {
+                // error message here
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
